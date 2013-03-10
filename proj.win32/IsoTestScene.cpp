e5244f35d4f8a5d1f068282d649be65e80b2b888
delorenj <jaradd@gmail.com>
2013-03-10 19:58:00 -0400

    Added a test scene. Removed lua script from initialization

---
+++ proj.win32/IsoTestScene.cpp
@@ -0,0 +1,11 @@
+#include "IsoTestScene.h"
+
+
+IsoTestScene::IsoTestScene(void)
+{
+}
+
+
+IsoTestScene::~IsoTestScene(void)
+{
+}
---
+++ proj.win32/IsoTestScene.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "c:\users\jarad\cocos2d-2.0-x-2.0.4\cocos2dx\layers_scenes_transitions_nodes\ccscene.h"
+class IsoTestScene :
+	public CCScene
+{
+public:
+	IsoTestScene(void);
+	~IsoTestScene(void);
+};
+
