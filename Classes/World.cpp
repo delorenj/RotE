76af29027cbd7cab1bef9bd9aee8fefcc6411c4f
Jarad Delorenzo <jarad.delorenzo@warbyparker.com>
2013-04-12 07:20:11 -0400

    Implemented momentum scrolling by using Box2d

--- Classes/World.cpp
+++ Classes/World.cpp
@@ -12,11 +12,32 @@ World::World() {
     this->setTouchEnabled(true);
     pMap = CCTMXTiledMap::create(TMX_FILE);
     addChild(pMap, 1);
-
-	pViewport = CCNode::create();
-	pViewport->setPosition(500,200);
-	pMap->addChild(pViewport, 1);
-
+    b2Vec2 grav = b2Vec2(0, 0);
+    pWorld = new b2World(grav);
+    b2BodyDef mapBodyDef;
+    mapBodyDef.position.Set(0.0, 0.0);
+    mapBodyDef.type = b2_dynamicBody;
+    mapBodyDef.userData = pMap;
+    pBody = pWorld->CreateBody(&mapBodyDef);
+    b2FixtureDef pMapFix;
+    b2CircleShape mapHandle;
+    mapHandle.m_radius = 1.0f;
+    pMapFix.shape = &mapHandle;
+    pMapFix.density = 1.0f;
+    pBody->CreateFixture(&pMapFix);
+    mapBodyDef.type = b2_staticBody;
+    pMapFrictionBody = pWorld->CreateBody(&mapBodyDef);
+    
+    b2FrictionJointDef fjDef;
+    fjDef.collideConnected = true;
+    fjDef.localAnchorA.SetZero();
+    fjDef.localAnchorB.SetZero();
+    fjDef.maxForce = 40;
+    fjDef.bodyA = pBody;
+    fjDef.bodyB = pMapFrictionBody;
+    
+    pWorld->CreateJoint(&fjDef);
+    
 	worldBoundary = CCRectMake(	0,
 								0,
 								pMap->getTileSize().width*getWorldSize().width, 
@@ -24,8 +45,6 @@ World::World() {
 
 	elapsedTime = 0;
 	scheduleUpdate();
-	//scrollAction = CCFollow::create(pViewport, worldBoundary);
-	//runAction(scrollAction);	
 }
 
 World::~World() {
@@ -43,17 +62,24 @@ CCSize World::getWorldSize() {
 }
 
 void World::update(float dt) {
-	elapsedTime += dt;	
+	elapsedTime += dt;
+    
+    pWorld->Step(dt, 10, 10);
+    for(b2Body *b = pWorld->GetBodyList(); b; b=b->GetNext()) {
+        if (b->GetUserData() != NULL) {
+            CCSprite *obj = (CCSprite *)b->GetUserData();
+            obj->setPosition(ccp(b->GetPosition().x * PTM_RATIO,
+                                 b->GetPosition().y * PTM_RATIO));
+        }
+    }
 }
 
 void World::ccTouchesBegan(CCSet *touches, CCEvent* event) {
     for(auto it = touches->begin(); it != touches->end(); it++) {
         CCTouch* touch = dynamic_cast<CCTouch*>(*it);
         if(touch) {
-			prevTouchTime = elapsedTime;
-			oldPos = pMap->getPosition();
-			newPos = oldPos;
-			touchOffset = ccpSub(oldPos,touchToPoint(touch));			
+            b2Vec2 pos = pBody->GetPosition();
+			touchOffset = ccpSub(ccp(pos.x*PTM_RATIO, pos.y*PTM_RATIO),touchToPoint(touch));
         }
     }
 }
@@ -62,21 +88,39 @@ void World::ccTouchesMoved(CCSet* touches, CCEvent* event) {
     for(auto it = touches->begin(); it != touches->end(); it++) {
         CCTouch* touch = dynamic_cast<CCTouch*>(*it);
         if(touch && touchOffset.x && touchOffset.y) {
-			oldPos = pMap->getPosition();
-			newPos = ccpAdd(this->touchToPoint(touch), this->touchOffset);
-			pMap->setPosition(newPos);			
+            lastPos = pBody->GetPosition();
+			CCPoint newpos = ccpAdd(touchToPoint(touch), touchOffset);
+//            b2Vec2 linearVelocity = b2Vec2(newpos.x/PTM_RATIO, newpos.y/PTM_RATIO) - pBody->GetPosition();
+//            pBody->SetLinearVelocity(linearVelocity);
+            pBody->SetTransform(b2Vec2(newpos.x/PTM_RATIO, newpos.y/PTM_RATIO), pBody->GetAngle());
+
         }
     }
 }
 
 void World::ccTouchesEnded(CCSet* touches, CCEvent* event) {
-	float denom = (elapsedTime - prevTouchTime);
-	denom = denom==0 ? 0.0001 : denom;
-	float mag = ccpDistance(oldPos, newPos) / denom;
-	CCLOG("Mag = %f", mag);
-	CCMoveTo* move = CCMoveTo::create(1.0f, ccpMult(newPos, clampf(mag,0,30)));
-	CCEaseOut* ease = CCEaseOut::create(move, 2);
-	pMap->runAction(ease);
-
+    for(auto it = touches->begin(); it != touches->end(); it++) {
+        CCTouch* touch = dynamic_cast<CCTouch*>(*it);
+        if(touch && touchOffset.x && touchOffset.y) {
+            b2Vec2 linearVelocity = b2Vec2(lastPos - pBody->GetPosition());
+            linearVelocity *= linearVelocity.Normalize() * -1 * 15;
+            pBody->SetLinearVelocity(linearVelocity);
+        }
+    }
+    
+//	float denom = (elapsedTime - prevTouchTime);
+//	denom = denom==0 ? 0.0001 : denom;
+//	float mag = ccpDistance(oldPos, newPos) / denom;
+//	CCLOG("Mag = %f", mag);
+//    
+//    CCPoint direction = ccpNormalize(ccp(oldPos.x - newPos.x, oldPos.y - newPos.y));
+//    CCPoint driftPos = ccpMult(direction, -40);
+//	CCMoveTo* move = CCMoveTo::create(1.0f, driftPos);
+//	CCEaseOut* ease = CCEaseOut::create(move, 2);
+//	pMap->runAction(ease);
+//
+//    CCLOG("direction = %f,%f", direction.x, direction.y);
+//    direction = ccpMult(direction, clampf(mag, 0, 30));
+//    CCLOG("direction*clamp = %f,%f", direction.x, direction.y);
 
 }
--- Classes/World.h
+++ Classes/World.h
@@ -10,8 +10,15 @@
 #define __RotE__World__
 
 #include <cocos2d.h>
+#include <Box2D/Box2D.h>
 
+#ifdef __APPLE__
+#define TMX_FILE "isometric_grass_and_water.tmx"
+#else
 #define TMX_FILE "../Resources/isometric_grass_and_water.tmx"
+#endif
+
+#define PTM_RATIO 32.0
 
 using namespace cocos2d;
 
@@ -28,17 +35,16 @@ public:
 	virtual void update(float dt);
     
 protected:
+    b2World* pWorld;
+    b2Body* pBody;
+    b2Body* pMapFrictionBody;
+    b2Vec2 lastPos;
+    float lastTime;
+    b2FrictionJoint* pFrictionJoint;
+    b2MassData mass;
     CCTMXTiledMap* pMap;
     CCPoint touchOffset;
-	CCPoint prevTouchOffset;
-	CCPoint oldPos;
-	CCPoint newPos;
-    CCNode* pViewport;
-	CCFollow* scrollAction;
 	CCRect worldBoundary;
 	float elapsedTime;
-	float curTouchTime;
-	float prevTouchTime;		
-    
 };
 #endif /* defined(__RotE__World__) */
